727ea13a70b7784fcccbe14cce5e544df80147fa
Kelly Carmichael <digikidd@gmail.com>
2016-10-17 22:12:25 -0500

    code updates

--- js/abcscript.js
+++ js/abcscript.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
 
 
-  $(window).scroll(function () {
-      
-  	var height = $(window).height();
-   
-    if ($(window).scrollTop() > height) {
-      $('#navbar').addClass('navbar-fixed');
+    $(window).scroll(function() {
+
+        var height = $(window).height();
+
+        if ($(window).scrollTop() > height) {
+            $('#navbar').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < height + 1) {
+            $('#navbar').removeClass('navbar-fixed');
+        }
+    });
+
+
+    function scrollToAnchor(index) {
+        var aTag = $("a[name='" + index + "']");
+        $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
     }
-    if ($(window).scrollTop() < height + 1) {
-      $('#navbar').removeClass('navbar-fixed');
-    }
-  });
 
-
-function scrollToAnchor(index){
-    var aTag = $("a[name='"+ index +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-}
-
-$(".arrow").click(function() {
-   scrollToAnchor('anchor1');
-});
+    $(".arrow").click(function() {
+        scrollToAnchor('anchor1');
+    });
 
 
 
