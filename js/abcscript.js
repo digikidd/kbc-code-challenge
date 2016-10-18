f94dc9bd01be1eabf940ace37efe4f3fc8fa639c
Kelly Carmichael <digikidd@gmail.com>
2016-10-17 20:15:49 -0500

    no message

--- js/abcscript.js
+++ js/abcscript.js
@@ -1,15 +1,27 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-		$(window),scroll(function() {
-			var wScroll = $(window).scrollTop();
 
-			if (wScroll > 100) {
+  $(window).scroll(function () {
+      
+      var height = $(window).height();
+   
+    if ($(window).scrollTop() > height) {
+      $('#navbar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < height + 1) {
+      $('#navbar').removeClass('navbar-fixed');
+    }
+  });
 
-			}
-			else {
-				
-			}
-		});
 
-        
+
+
+
+
+
+
+
+
+
+
 });
