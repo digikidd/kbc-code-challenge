53b2475452318fc4ae61f14491cc1056ed869f24
Kelly Carmichael <digikidd@gmail.com>
2016-10-18 22:21:48 -0500

    no message

--- js/abcscript.js
+++ js/abcscript.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
 
+    	showImages('.girl');
+
         var height = $(window).height();
 
         if ($(window).scrollTop() > height) {
@@ -14,19 +16,33 @@ $(document).ready(function() {
     });
 
 
-    function scrollToAnchor(index) {
-        var aTag = $("a[name='" + index + "']");
+    function scrollToAnchor(e) {
+        var aTag = $("a[name='" + e + "']");
         $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
-    }
+    };
+
 
     $(".arrow").click(function() {
         scrollToAnchor('anchor1');
     });
 
 
+    function showImages(e) {
+        var windowHeight = jQuery(window).height();
+        $(e).each(function() {
+            var thisPos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (topOfWindow + windowHeight - 200 > thisPos) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    };
+    
+    showImages('.girl');
 
 
-
+    
 
 
 });
