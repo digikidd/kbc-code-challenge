3e77e76ae8b5d4177e826a2951ba74cceed936c2
Kelly Carmichael <digikidd@gmail.com>
2016-10-19 15:10:16 -0500

    code updates

--- js/abcscript.js
+++ js/abcscript.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
 
-    	showImages('.girl');
+    	showImages('#girl');
 
         var height = $(window).height();
 
@@ -31,15 +31,21 @@ $(document).ready(function() {
         var windowHeight = jQuery(window).height();
         $(e).each(function() {
             var thisPos = $(this).offset().top;
+            console.log(this);
 
             var topOfWindow = $(window).scrollTop();
             if (topOfWindow + windowHeight - 200 > thisPos) {
                 $(this).addClass("fadeIn");
+
             }
+            else {
+            	$(this).removeClass('fadeIn');
+            }
+
         });
     };
     
-    showImages('.girl');
+    showImages('#girl');
 
 
     
